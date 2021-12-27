89a56eb1515aa772623cf609879f225aa3ce4f65
markpots5 <m.h.pots@outlook.com>
2021-12-27 15:45:13 +0100

    test

--- cli.js
+++ cli.js
@@ -5,3 +5,15 @@ const [,, ...args] = process.argv
 const { spawn } = require('child_process');
 const bat = spawn('cmd.exe', ['/c', 'main.bat']);
 
+bat.stdout.on('data', (data) => {
+  console.log(data.toString());
+});
+
+bat.stderr.on('data', (data) => {
+  console.error(data.toString());
+});
+
+bat.on('exit', (code) => {
+  console.log(`Child exited with code ${code}`);
+});
+
